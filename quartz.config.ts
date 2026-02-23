463202f4b4e60285537624f4cea752bbbc7dec88
zhuangqh <zhuangqhc@gmail.com>
2026-02-23 20:07:14 +1100

    fix baseurl
    Signed-off-by: zhuangqh <zhuangqhc@gmail.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "zhuangqh.github.io/blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
